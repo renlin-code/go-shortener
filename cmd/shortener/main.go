54dfd643792db5f1b8a0eb780cddfd8ee2913584
Rene <renelj1997@gmail.com>
2024-06-22 20:53:15 +0500

    feat: added logger

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/renlin-code/go-shortener/internal/config"
 )
 
@@ -9,9 +12,41 @@ func main() {
 
 	//TODO: init logger (slog)
 
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages enabled")
+
 	//TODO: init storage (SQL Lite)
 
 	//TODO: init router (chi)
 
 	//TODO: run server
+}
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
