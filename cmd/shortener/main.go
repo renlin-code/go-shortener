740676b8a181ec8ad84e1dde4a798772266f96d1
Rene <renelj1997@gmail.com>
2024-06-24 12:59:25 +0500

    feat: added styles handler for slog in local environment

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,7 +4,12 @@ import (
 	"log/slog"
 	"os"
 
+	mwLogger "github.com/renlin-code/go-shortener/internal/http-server/middleware/logger"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/renlin-code/go-shortener/internal/config"
+	"github.com/renlin-code/go-shortener/internal/lib/logger/handlers/slogpretty"
 	sl "github.com/renlin-code/go-shortener/internal/lib/logger/slog"
 	"github.com/renlin-code/go-shortener/internal/storage/sqlite"
 )
@@ -17,13 +22,18 @@ func main() {
 	log.Info("starting shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages enabled")
 
-	storage, err := sqlite.NewStorage(cfg.StoragePath)
+	_, err := sqlite.NewStorage(cfg.StoragePath)
 	if err != nil {
 		log.Error("failed to init storage", sl.Err(err))
 		os.Exit(1)
 	}
 
-	//TODO: init router (chi)
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 
 	//TODO: run server
 }
@@ -39,9 +49,7 @@ func setupLogger(env string) *slog.Logger {
 
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
@@ -53,4 +61,16 @@ func setupLogger(env string) *slog.Logger {
 	}
 
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
